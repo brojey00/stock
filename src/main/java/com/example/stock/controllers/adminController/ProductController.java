b3267a67e84ea06d169f0c2e05eea7cd9ff01563
brojeyo <elhamile.hatim@ensam-casa.ma>
2026-01-27 21:52:03 +0100

    started the controller

--- src/main/java/com/example/stock/controllers/adminController/ProductController.java
+++ src/main/java/com/example/stock/controllers/adminController/ProductController.java
@@ -1,7 +1,33 @@
 package com.example.stock.controllers.adminController;
 
+import com.example.stock.dao.entities.Entrepot;
+import com.example.stock.dao.entities.Product;
+import com.example.stock.service.EntropotManager;
+import com.example.stock.service.ProductManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@PreAuthorize("hasRole('ADMIN')")
 @RestController
 public class ProductController {
+    @Autowired
+    private ProductManager productManager;
+    @GetMapping("/products")
+    public List<Product> showProducts(){
+        List<Product> list = productManager.getAll();
+        if ((int) list.size() ==0){
+            return List.of();
+        }
+        return list;
+    }
+    @PostMapping("/addproduct")
+    public Product addproduct(){
+
+    }
+
 }
--- src/main/java/com/example/stock/service/EntropotManager.java
+++ src/main/java/com/example/stock/service/EntropotManager.java
@@ -1,11 +1,17 @@
 package com.example.stock.service;
 
+import com.example.stock.dao.entities.Entrepot;
 import com.example.stock.dao.repositories.EntropotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EntropotManager {
     @Autowired
     private EntropotRepository entropotRepository;
+    public List<Entrepot> getAll(){
+        return entropotRepository.findAll();
+    }
 }
